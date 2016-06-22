f9d4d0a3ff30e9c983f929e7c3989949ef553afc
LiuyangSB <18623912606@qq.com>
2016-06-22 10:12:16 +0800

    123
    321

---
+++ IT007GitHubDemo/IT007GitHubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IT007GitHubDemo
+//
+//  Created by Mac on 16/6/22.
+//  Copyright © 2016年 LvShuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IT007GitHubDemo/IT007GitHubDemoTests/IT007GitHubDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  IT007GitHubDemoTests.m
+//  IT007GitHubDemoTests
+//
+//  Created by Mac on 16/6/22.
+//  Copyright © 2016年 LvShuai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IT007GitHubDemoTests : XCTestCase
+
+@end
+
+@implementation IT007GitHubDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
